3dab183ce35a4a74fd191756375774f86a71fee9
Robbie McLeod <31489752+rbbi@users.noreply.github.com>
2024-08-06 22:37:35 +0100

    Upgrade JUnit dependency, refactoring and add info to README.md
    * Upgrade junit 4.12 => 4.13.2
    * Add notification behaviour to README.md
    * Update tests
    * Bump to v1.1.2

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Seaweed Spore Notifier[<img align="right" height="64" src="https://raw.githubusercontent.com/rbbi/seaweed-spore-notifier/master/icon.png">](icon.png)
+
 [![](https://img.shields.io/endpoint?url=https://i.pluginhub.info/shields/installs/plugin/seaweed-spore-notifier)](https://runelite.net/plugin-hub/show/seaweed-spore-notifier)
 
 Notifies you when seaweed spores spawn.
+
+Notification behaviour is managed by RuneLite's own Notification Settings - review these in advance to ensure you get
+notified when you expect to!
--- src/test/java/io/github/rbbi/plugin/SeaweedSporeNotifierPluginUnitTests.java
+++ src/test/java/io/github/rbbi/plugin/SeaweedSporeNotifierPluginUnitTests.java
@@ -1,23 +1,21 @@
 package io.github.rbbi.plugin;
 
 import net.runelite.api.ItemID;
-import net.runelite.api.Model;
-import net.runelite.api.Node;
 import net.runelite.api.TileItem;
 import net.runelite.api.events.ItemSpawned;
 import net.runelite.client.Notifier;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.clearInvocations;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.mockito.junit.MockitoJUnitRunner;
 
 /**
+ * Tests for {@link SeaweedSporeNotifierPlugin}
+ *
  * @author robbie, created on 09/08/2022 21:56
  */
 @RunWith(MockitoJUnitRunner.class)
@@ -39,21 +37,24 @@ public class SeaweedSporeNotifierPluginUnitTests
 		when(config.minQuantity()).thenReturn(1);
 		plugin.onItemSpawned(newItemWithQuantity(ItemID.SEAWEED_SPORE, 1));
 		verify(notifier, times(1)).notify("1 seaweed spore has spawned!");
-		clearInvocations(notifier);
+	}
 
-		// testSpawnedQuantitySameAsConfigMinimumNotifies
+	@Test
+	public void testSpawnedQuantitySameAsConfigMinimumNotifies() {
 		when(config.minQuantity()).thenReturn(2);
 		plugin.onItemSpawned(newItemWithQuantity(ItemID.SEAWEED_SPORE, 2));
 		verify(notifier, times(1)).notify("2 seaweed spores have spawned!");
-		clearInvocations(notifier);
+	}
 
-		// testSpawnedQuantityAboveConfigMinimumNotifies
+	@Test
+	public void testSpawnedQuantityAboveConfigMinimumNotifies() {
 		when(config.minQuantity()).thenReturn(2);
 		plugin.onItemSpawned(newItemWithQuantity(ItemID.SEAWEED_SPORE, 3));
 		verify(notifier, times(1)).notify("3 seaweed spores have spawned!");
-		clearInvocations(notifier);
+	}
 
-		// testSpawnedQuantityBelowConfigMinimumDoesntNotify
+	@Test
+	public void testSpawnedQuantityBelowConfigMinimumDoesntNotify() {
 		when(config.minQuantity()).thenReturn(3);
 		plugin.onItemSpawned(newItemWithQuantity(ItemID.SEAWEED_SPORE, 2));
 		verify(notifier, times(0)).notify(any());
@@ -68,62 +69,10 @@ public class SeaweedSporeNotifierPluginUnitTests
 
 	private ItemSpawned newItemWithQuantity(int itemId, int quantity)
 	{
-		return new ItemSpawned(null, new TileItem()
-		{
-			@Override
-			public int getId()
-			{
-				return itemId;
-			}
-
-			@Override
-			public int getQuantity()
-			{
-				return quantity;
-			}
-
-			@Override
-			public Model getModel()
-			{
-				return null;
-			}
-
-			@Override
-			public int getModelHeight()
-			{
-				return 0;
-			}
-
-			@Override
-			public void setModelHeight(int modelHeight)
-			{
-
-			}
-
-			@Override
-			public void draw(int orientation, int pitchSin, int pitchCos, int yawSin, int yawCos, int x, int y, int z, long hash)
-			{
-
-			}
-
-			@Override
-			public Node getNext()
-			{
-				return null;
-			}
-
-			@Override
-			public Node getPrevious()
-			{
-				return null;
-			}
-
-			@Override
-			public long getHash()
-			{
-				return 0;
-			}
-		});
+		TileItem tileItem = mock(TileItem.class);
+		when(tileItem.getId()).thenReturn(itemId);
+		when(tileItem.getQuantity()).thenReturn(quantity);
+		return new ItemSpawned(null, tileItem);
 	}
 
 }
